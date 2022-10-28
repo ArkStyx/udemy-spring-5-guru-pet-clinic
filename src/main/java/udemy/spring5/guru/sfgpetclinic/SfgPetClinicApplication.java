920e6ea9c55df36de21ed552733505bdfbddab58
ArkStyx <regis.petit@gmail.com>
2022-10-29 00:04:58 +0200

    Test import via repertoire

---
+++ src/main/java/udemy/spring5/guru/sfgpetclinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package udemy.spring5.guru.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicApplication.class, args);
+	}
+
+}
