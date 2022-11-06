478975b3388602d33fff862b233ec062316e4ba2
ArkStyx <regis.petit@gmail.com>
2022-11-06 18:19:09 +0100

    Ajout de la page d'index et du controller pour les veterinaires. Closes #7

---
+++ sfg-pet-clinic-web/src/main/java/udemy/spring5/guru/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,15 @@
+package udemy.spring5.guru.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+	
+	@RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+	public String listVets() {
+		
+		return "vets/index";
+	}
+}
