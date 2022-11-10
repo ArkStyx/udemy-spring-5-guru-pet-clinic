bb3f342c32c280873d16b24d1b586f183aa892ee
ArkStyx <regis.petit@gmail.com>
2022-11-10 22:51:14 +0100

    Ajout du model dans le controller owner. Closes #25

--- sfg-pet-clinic-web/src/main/java/udemy/spring5/guru/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic-web/src/main/java/udemy/spring5/guru/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,32 @@
 package udemy.spring5.guru.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import udemy.spring5.guru.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"", "/", "/index", "/index.html"})
-	public String listOwners() {
+	public String listOwners(Model model) {
 		
-		return "owners/index";
+		String nomAttributDansTemplateThymeleaf = "listeProprietaires";
+		model.addAttribute(nomAttributDansTemplateThymeleaf, ownerService.findAll());
+		
+		String nomRepertoireThymeleaf = "owners";
+		String nomTemplateThymeleaf = "index";
+		String modelPourRetour = nomRepertoireThymeleaf + "/" + nomTemplateThymeleaf;
+		
+		return modelPourRetour;
 	}
 }
