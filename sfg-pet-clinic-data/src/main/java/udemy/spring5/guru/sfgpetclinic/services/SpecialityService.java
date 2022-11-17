8afccf0cf7dc2d031d8b8897b6cc191bd68cc292
ArkStyx <regis.petit@gmail.com>
2022-11-17 23:28:08 +0100

    Ajout map specialite pour les veterinaires

---
+++ sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package udemy.spring5.guru.sfgpetclinic.services;
+
+import udemy.spring5.guru.sfgpetclinic.models.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+
+}
