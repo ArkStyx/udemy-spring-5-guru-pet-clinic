6245c44f3d40d3e1fde892dc953e99333b14da8e
ArkStyx <regis.petit@gmail.com>
2022-11-27 18:15:13 +0100

    Ajout entites JPA Vet et Speciality

--- sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/Speciality.java
+++ sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/Speciality.java
@@ -1,12 +1,19 @@
 package udemy.spring5.guru.sfgpetclinic.models;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 import udemy.spring5.guru.sfgpetclinic.models.base.BaseEntity;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_speciality")
 public class Speciality extends BaseEntity {
 
+	@Column(name = "description")
 	private String description;
 }
--- sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/Vet.java
+++ sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/Vet.java
@@ -3,13 +3,30 @@ package udemy.spring5.guru.sfgpetclinic.models;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
+import net.bytebuddy.utility.nullability.MaybeNull;
 import udemy.spring5.guru.sfgpetclinic.models.base.BasePerson;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_vet")
 public class Vet extends BasePerson {
 
+	/*
+	 * TODO Si on laisse le FetchType par defaut, qui est de LAZY pour @ManyToMany, alors les specialites ne seront pas charges sur appel de l'objet Vet
+	 */
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "tb_vet_speciality", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
 	private Set<Speciality> specialities = new LinkedHashSet<>();
 }
