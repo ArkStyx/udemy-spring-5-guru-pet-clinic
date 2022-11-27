60a9fc462a7b5c88edc93b51fb2690057dabad3a
ArkStyx <regis.petit@gmail.com>
2022-11-27 18:27:01 +0100

    Ajout entite JPA Visit et modif Pet. Closes #47

--- sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/Pet.java
+++ sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/Pet.java
@@ -1,11 +1,15 @@
 package udemy.spring5.guru.sfgpetclinic.models;
 
 import java.time.LocalDate;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Getter;
@@ -31,4 +35,10 @@ public class Pet extends BaseEntity {
 	@ManyToOne
 	@JoinColumn(name = "owner_id")
 	private Owner owner;
+	
+	/*
+	 * TODO si on supprime un animal, on supprime aussi ses visites
+	 */
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new LinkedHashSet<>();
 }
--- sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/Visit.java
+++ sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/Visit.java
@@ -2,15 +2,29 @@ package udemy.spring5.guru.sfgpetclinic.models;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 import udemy.spring5.guru.sfgpetclinic.models.base.BaseEntity;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tb_visit")
 public class Visit extends BaseEntity {
 
+	@Column(name = "date")
 	private LocalDate date;
+	
+	@Column(name = "description")
 	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 }
