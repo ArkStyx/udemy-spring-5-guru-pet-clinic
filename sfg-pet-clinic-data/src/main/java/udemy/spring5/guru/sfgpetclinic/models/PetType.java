31528b98c640cbb9c3682798e85e4404497b0cfc
ArkStyx <regis.petit@gmail.com>
2023-02-23 23:42:15 +0100

    Adding Builder Pattern to PetType. #65

--- sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/PetType.java
+++ sfg-pet-clinic-data/src/main/java/udemy/spring5/guru/sfgpetclinic/models/PetType.java
@@ -4,16 +4,27 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import udemy.spring5.guru.sfgpetclinic.models.base.BaseEntity;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "tb_pet_type")
 public class PetType extends BaseEntity {
+
+	@Builder
+	public PetType(Long id, String name) {
+		super(id);
+		this.name = name;
+	}
 
 	@Column(name = "name")
 	private String name;
